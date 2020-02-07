57c78fe603afb03754a703f6ad2b72b6aaaa3dbd
abl3l <7ckngMad.mn@gmail.com>
2020-02-07 14:48:38 +0200

    ImageFinder

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,7 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const searchByQuery = (query = "", page = 1) => {
+export const searchByQuery = (query = '', page = 1) => {
   return axios.get(
-    `https://pixabay.com/api/?key=14922494-aee89178795b078ccc72cd443&orientation=horizontal&q=${query}&per_page=12&page=${page}`
+    `https://pixabay.com/api/?key=14922494-aee89178795b078ccc72cd443&orientation=horizontal&q=${query}&per_page=12&page=${page}`,
   );
 };
+
+export const a = 2 + 3;
